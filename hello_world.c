038cb378cc2209b286ff38ddb7a255b3ddea4a00
hibou42 <hibou42@protonmail.com>
2022-07-27 01:58:13 +0200

    excellent

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int	main (void)
 {
 	printf("Hello World\n");
+//Et puisse la norminette vous être favorable	
+	int	x = 0;
+	char	c[] = "Made from my ThinkPadE590";
+	while (c[x] != '\0')
+	{
+		write (1, &c[x], 1);
+		x++;
+	}
+	write (1, "\n", 1);
 	return (0);
 }
