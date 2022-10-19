80c2197ac68b7e5912cc5e44bf5fd3e9a692ef9b
hibou42 <110067148+hibou42@users.noreply.github.com>
2022-10-19 21:01:02 +0200

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -6,7 +6,7 @@ int	main (void)
 	printf("Hello World\n");
 //Et puisse la norminette vous être favorable	
 	int	x = 0;
-	char	c[] = "Made from my ThinkPadE590";
+	char	c[] = "Made from my ThinkPad";
 	while (c[x] != '\0')
 	{
 		write (1, &c[x], 1);
